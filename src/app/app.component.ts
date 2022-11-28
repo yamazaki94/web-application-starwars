b687b68df7a9f904aeaf03a437f39d6737a536c3
shanlakshitha94 <shanlakshitha94@hotmail.com>
2022-11-28 10:49:33 +0530

    Bug Fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StarWarsService } from './services/star-wars.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(
+    private starWarsApi : StarWarsService
+  ) { }
+
+  ngOnInit(): void {
+    this.starWarsApi.getFilms().subscribe(
+      res => {
+        this.starWarsApi._filmsList$.next(res)
+      }
+    )
+  }
   title = 'sort-web-app';
 }
